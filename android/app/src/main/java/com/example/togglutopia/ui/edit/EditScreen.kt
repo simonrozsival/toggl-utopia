23a7ee33ae4d4237c97fe8c84470608676f1e222
tomas <semanticer@gmail.com>
2019-12-17 20:30:12 +0100

    Broken project selection

--- android/app/src/main/java/com/example/togglutopia/ui/edit/EditScreen.kt
+++ android/app/src/main/java/com/example/togglutopia/ui/edit/EditScreen.kt
@@ -1,21 +1,44 @@
 package com.example.togglutopia.ui.edit
 
 import androidx.compose.Composable
-import androidx.ui.core.Text
+import androidx.compose.state
+import androidx.compose.unaryPlus
+import androidx.ui.core.dp
+import androidx.ui.layout.Column
+import androidx.ui.layout.Spacing
+import androidx.ui.material.RadioGroup
+import androidx.ui.material.surface.Card
 import androidx.ui.tooling.preview.Preview
 import com.example.togglutopia.data.model.TimeEntry
-import com.example.togglutopia.ui.LogInteractions
-import com.example.togglutopia.ui.TogglApp
 import com.example.togglutopia.ui.TogglState
-import com.example.togglutopia.ui.log.LogScreen
 import com.example.togglutopia.ui.timeEntry.TimeEntryContent
 
 @Composable
 fun EditScreen(timeEntryId: Int) {
-    val editedTimeEntry = TogglState.timeEntryList.find { it.id == timeEntryId }
-    editedTimeEntry?.apply {
-        TimeEntryContent(this, true)
+    val editedTimeEntry = TogglState.timeEntryList.find { it.id == timeEntryId } ?: return
+
+    Column(modifier = Spacing(16.dp)){
+        TimeEntryContent(editedTimeEntry, true)
+        if (!TogglState.projectList.isEmpty()) {
+            ProjectSelector(editedTimeEntry)
+        }
     }
+
+}
+
+@Composable
+fun ProjectSelector(timeEntry: TimeEntry) {
+    val projectOptions = TogglState.projectList.filter { it.active }.map { it.name }
+    val selectedProject = TogglState.projectList.find { it.id == timeEntry.project_id }
+    val (selectedOption, _) = +state { selectedProject?.name }
+    RadioGroup(
+            options = projectOptions,
+            selectedOption = selectedOption,
+            onSelectedChange = { projectName ->
+                val project = TogglState.projectList.first { it.name == projectName }
+                TogglState.localEditTimeEntry(timeEntry.copy(project_id = project.id))
+            }
+    )
 }
 
 @Preview
@@ -26,4 +49,6 @@ fun preview() {
     ))
     EditScreen(1)
 }
+
+
 
